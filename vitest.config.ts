2a7fd07e9ba5190d4e5734296bed682b8622238e
Ralf Traunsteiner <ralf.traunsteiner@strg.at>
2025-06-02 17:42:06 +0200

    fix: docker-ignore, INPRO-2706

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,7 @@ export default mergeConfig(
   defineConfig({
     test: {
       environment: 'jsdom',
+      globalSetup: './vitest.globals.ts',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
     },
